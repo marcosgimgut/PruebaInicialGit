a58e67284079fdb17617a57b4c9a916a4bc9626e
Marcos G <marcosgimeno@gmail.com>
2021-07-21 09:53:19 +0200

    He creado un proyecto

--- src/main/java/com/mycompany/preubagit/ClasePrueba.java
+++ src/main/java/com/mycompany/preubagit/ClasePrueba.java
@@ -15,6 +15,17 @@ public class ClasePrueba {
         return a+b;
     }
     
+    public int resta (int a, int b){
+        return a-b;
+    }
+    
+    public int multiplicacion (int a, int b){
+        return a*b;
+    }
+    
+    public int division (int a, int b){
+        return a/b;
+    }
     
     
 }
