a03913c6bb834eb9668447877414f912268ddd9e
Marcos G <marcosgimeno@gmail.com>
2021-07-21 09:42:17 +0200

    He creado un proyecto

---
+++ src/main/java/com/mycompany/preubagit/ClasePrueba.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.preubagit;
+
+/**
+ *
+ * @author Marcos
+ */
+public class ClasePrueba {
+    
+    public int suma (int a, int b){
+        return a+b;
+    }
+    
+    
+    
+}
